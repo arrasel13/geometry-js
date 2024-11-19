25a6e3fd3a56543974c8900ff210e1b72f1d7dc9
AR Rasel <rasel@wpdeveloper.com>
2024-11-19 14:18:40 +0600

    Rectangle calculation done

--- scripts/rectangle-calculate.js
+++ scripts/rectangle-calculate.js
@@ -1,3 +1,12 @@
 function calculateRectangleArea() {
-    console.log('first')
+    const rectangleWidthInput = document.getElementById('rectangle-width');
+    const rectangleWidth = parseFloat(rectangleWidthInput.value);
+    
+    const rectangleLengthInput = document.getElementById('rectangle-length');
+    const rectangleLength = parseFloat(rectangleLengthInput.value);
+
+    const area = rectangleWidth * rectangleLength;
+
+    const rectangleArea = document.getElementById('rectangle-area');
+    rectangleArea.innerText = area;
 }
